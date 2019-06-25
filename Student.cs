ee0986b7e0bec036422d5247fcfad21247ac9566
Rawat Amardeep ArjunPrasad <47588491+Amardeep9@users.noreply.github.com>
2019-06-25 15:25:24 +0530

    Add files via upload

---
+++ Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TypedandUntypedView.Models
+{
+    public class Student
+    {
+        public int StudentID { get; set; }
+        public string Name { get; set; }
+        public long MobileNo { get; set; }
+        public string Address { get; set; }
+
+        public string Image { get; set; }
+    }
+}
---
+++ StudentController.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TypedandUntypedView.Models;
+
+namespace TypedandUntypedView.Controllers
+{
+    public class StudentController : Controller
+    {
+        
+        // GET: Student
+        public ActionResult Typed()
+        {
+            Student objstu = new Student();
+            objstu.StudentID = 1001;
+            objstu.Name = "Amardeep Kumar";
+            objstu.MobileNo = 9974223439;
+            objstu.Address = "Vapi";
+            objstu.Image= "F:/Practicals/TypedViewandUntypedView/TypedandUntypedView/Image/jio.jpg";
+            return View(objstu);
+        }
+
+        public ActionResult ListTyped()
+        {
+            List<Student> lststu = new List<Student>();
+            Student objstu = new Student();
+            objstu.StudentID = 1001;
+            objstu.Name = "Amardeep Kumar";
+            objstu.MobileNo = 9974223439;
+            objstu.Address = "Vapi";
+            //return View(objstu);
+            lststu.Add(objstu);
+            return  View(lststu);
+        }
+
+
+        public ActionResult ListUnTyped()
+        {
+            List<Student> lststu = new List<Student>();
+            Student objstu = new Student();
+            objstu.StudentID = 1001;
+            objstu.Name = "Amardeep Kumar";
+            objstu.MobileNo = 9974223439;
+            objstu.Address = "Vapi";
+            //return View(objstu);
+            lststu.Add(objstu);
+            ViewBag.StuLst = lststu;
+            return View(lststu);
+        }
+
+
+
+        public ActionResult ListUnTypedViewData()
+        {
+            List<Student> lststu = new List<Student>();
+            Student objstu = new Student();
+            objstu.StudentID = 1001;
+            objstu.Name = "Amardeep Kumar";
+            objstu.MobileNo = 9974223439;
+            objstu.Address = "Vapi";
+            //return View(objstu);
+            lststu.Add(objstu);
+            ViewData["Students"] = lststu;
+            return View(lststu);
+        }
+
+
+
+        public ActionResult Strongly()
+        {
+            Student objstu = new Student();
+            objstu.StudentID = 1001;
+            objstu.Name = "Amardeep Kumar";
+            objstu.MobileNo = 9974223439;
+            objstu.Address = "Vapi";
+            return View(objstu);
+        }
+
+
+        public ActionResult Untyped()
+        {
+            Student objstu = new Student();
+            objstu.StudentID = 1001;
+            objstu.Name = "Amardeep Kumar";
+            objstu.MobileNo = 9974223439;
+            objstu.Address = "Vapi";
+
+             ViewBag.Logo = @"C:\Users\admin\Pictures\New folder\jio.png";
+            // ViewBag.Logo = Server.MapPath("~") + @"C:\Users\admin\Pictures\New folder\jio.png";
+            //ViewBag.Logo = Url.Content("C:/Users/admin/Pictures/New folder/jio.png");
+
+            ViewBag.Stud = objstu;
+            return View();
+        }
+
+
+    }
+}
